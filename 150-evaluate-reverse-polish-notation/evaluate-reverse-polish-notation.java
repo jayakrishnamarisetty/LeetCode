afe09b8b51e6b73559f4d2c7ce4586728f8683d9
M JAYA KRISHNA SAI TEJA <119914396+jayakrishnamarisetty@users.noreply.github.com>
2024-01-30 18:49:15 +0530

    Time: 2 ms (99.46%) | Memory: 44.9 MB (20.90%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
@@ -1,28 +1,28 @@
 class Solution {
     public int evalRPN(String[] tokens) {
-        int[] stack = new int[tokens.length];
+        int[] rug = new int[tokens.length];
         int top = 0;
         for(String s : tokens) {
             char c = s.charAt(0);
             if(c=='+') {
-                int b = stack[--top];
-                int a = stack[--top];
-                stack[top++] = a+b;
+                int b = rug[--top];
+                int a = rug[--top];
+                rug[top++] = a+b;
             } else if(c=='-' && s.length()==1) {
-                int b = stack[--top];
-                int a = stack[--top];
-                stack[top++] = a-b;
+                int b = rug[--top];
+                int a = rug[--top];
+                rug[top++] = a-b;
             } else if(c=='*') {
-                int b = stack[--top];
-                int a = stack[--top];
-                stack[top++] = a*b;
+                int b = rug[--top];
+                int a = rug[--top];
+                rug[top++] = a*b;
             } else if(c=='/') {
-                int b = stack[--top];
-                int a = stack[--top];
-                stack[top++] = a/b;
+                int b = rug[--top];
+                int a = rug[--top];
+                rug[top++] = a/b;
             } else 
-                stack[top++] = Integer.parseInt(s);
+                rug[top++] = Integer.parseInt(s);
         }
-        return stack[0];
+        return rug[0];
     }
 }
